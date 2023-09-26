1847fb8d8001d76b00d07298f15538326e770258
yefry <yefryrodelo@gmail.com>
2023-09-25 19:13:11 -0500

    implementacion de seguridad, aun no esta terminada

--- src/main/java/com/api/rest/softlond/config/CorsConfig.java
+++ src/main/java/com/api/rest/softlond/config/CorsConfig.java
@@ -2,11 +2,14 @@ package com.api.rest.softlond.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 // Para no tener que anotar en cada uno de nuestros controller la anotacion @CrossOrigin(origins = "http://localhost:4200"), creamos este cors global
 
-/*@Configuration
+@Configuration
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -21,4 +24,4 @@ public class CorsConfig {
 
         return source;
     }
-}*/
+}
--- src/main/java/com/api/rest/softlond/config/SecurityConfig.java
+++ src/main/java/com/api/rest/softlond/config/SecurityConfig.java
@@ -3,25 +3,37 @@ package com.api.rest.softlond.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-/*@Configuration
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) ->
                         authorize
+                                .requestMatchers(HttpMethod.GET,"/api/product").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","CUSTOMER")
                                 .requestMatchers(HttpMethod.POST,"/api/**").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults());
 
                 return httpSecurity.build();
     }
@@ -48,4 +60,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-}*/
+}
